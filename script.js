962124acdb786bdc0ef4c504516e5bf82ea5795f
Begal Wadim <begal-vadim98@yandex.ru>
2022-01-27 11:58:52 +0300

    Выполненное домашнее задание

---
+++ script.js
@@ -0,0 +1,10 @@
+const  title = 0,
+       screens = 0,
+       screenPrice = 0,
+       rollback = 0,
+       fullPrice = 0,
+       adaptive = 0;
+
+alert('Привет мир!');
+
+console.log("alert отработал, теперь вывели это сообщение в консоль");
