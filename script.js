a61965d0110a3b51a6d51901747f078728534cdf
Begal Wadim <begal-vadim98@yandex.ru>
2022-01-28 11:35:18 +0300

    Выполненное домашнее задание №2

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const  title = 0,
-  screens = 0,
-  screenPrice = 0,
-  rollback = 0,
-  fullPrice = 0,
-  adaptive = 0;
+const  title = "Lesson #2",
+  screens = "Простые, Сложные, Интерактивные",
+  screenPrice = 100,
+  rollback = 70,
+  fullPrice = 3000,
+  adaptive = false;
 
-alert('Привет мир!');
+// alert('Привет мир!');
 
-console.log("alert отработал, теперь вывели это сообщение в консоль");
+console.log(`Типы данных: ${typeof title}, ${typeof fullPrice}, ${typeof adaptive}`);
+console.log(`Длина строки: ${screens.length}`);
+console.log(`Стоимость верстки экранов: ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта: ${fullPrice} рублей`);
+
+// Создаем функцию для перевода строки и разбивки ее на массив
+const funScreens = (element) => {
+  element = screens.toLowerCase();
+  element = element.split(",");
+  console.log(element);
+}
+funScreens();
+
+console.log(`Процент отката посреднику за работу: ${fullPrice * (rollback / 100)}`);
