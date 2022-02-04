4e09db0ad47bbf7ca2af2bc81bbf895daf3a1e3a
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-04 14:05:43 +0300

    Закинул файлы дз

--- script.js
+++ script.js
@@ -1,122 +1,122 @@
 'use strict'
 
-let  rollback = 10,
-  title,
-  screens,
-  screenPrice,
-  allServicePrices,
-  adaptive,
-  fullPrice,
-  servicePercentPrice,
-  additionalServiceOne,
-  additionalServiceTwo;
+const appData = {
+  rollback: 10,
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  allServicePrices: 0,
+  adaptive: true,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  additionalServiceOne: '',
+  additionalServiceTwo: '',
 
-const isNumber = function(num) {
-  return !isNaN(parseFloat(num))  && isFinite(num) && !num.includes(" ");
-}
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num) && !num.includes(" ");
+  },
 
-const asking = function () {
-  title = prompt("Как называется ваш проект?", "Lesson"),
-  screens = prompt("Какие типы экранов нужно разработать??", "Простые, Сложные, Интерактивные");
+  getAllServicePrices: function () {
+    let sum = 0,
+      sumNumber;
 
-  do {
-    screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
-  } while (!isNumber(screenPrice));
+    for (let i = 0; i < 2; i++) {
 
-  screenPrice = parseFloat(screenPrice);
+      if (i === 0) appData.additionalServiceOne = prompt("Какой дополнительный тип услуги необходим", `Услуга ${i + 1}`);
 
-  adaptive = confirm("Нужен ли адаптив на сайте?");
+      if (i === 1) appData.additionalServiceTwo = prompt("Какой дополнительный тип услуги необходим", `Услуга ${i + 1}`);
 
-}
-
-
-// Функция возвращает сумму всех дополнительных услуг
-const getAllServicePrices = function() {
-  let sum = 0,
-    sumNumber;
-
-  for (let i = 0; i < 2; i++) {
-
-      if(i === 0) additionalServiceOne = prompt("Какой дополнительный тип услуги необходим", `Услуга ${i + 1}`);
-
-      if(i === 1) additionalServiceTwo = prompt("Какой дополнительный тип услуги необходим", `Услуга ${i + 1}`);
-      
       do {
         sumNumber = prompt("Сколько это будет стоить?", "1000");
 
-      } while(!isNumber(sumNumber));
-      
+      } while (!appData.isNumber(sumNumber));
+
       sumNumber = parseFloat(sumNumber);
 
 
       sum += sumNumber;
 
-  } 
+    }
 
-  return sum;
+    return sum;
 
-}
+  },
 
-const showTypeOf = function(variables) {
+  getFullPrice: function (priceWork, amountServices) {
 
-  return `${variables} : ${typeof variables}`
+    return priceWork + amountServices;
 
-}
-// Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг 
-const getFullPrice = function (priceWork, amountServices) {
-  
-  return priceWork + amountServices;
+  },
 
-}
+  getServicePercentPrices: function (amount, percent) {
 
-// getServicePercentPrices
-const getServicePercentPrices = function(amount, percent) {
+    return (amount) - Math.ceil((amount * (percent / 100)));
 
-  return (amount) -  Math.ceil((amount * (percent / 100)));
+  },
 
-}
+  getTitle: function (documentTitle) {
 
-// Функция возвращает title меняя его 
-const getTitle = function(documentTitle) {
+    const removingSpaces = documentTitle.trim(),
+      OneLetter = removingSpaces[0].toUpperCase();
 
-  const removingSpaces = documentTitle.trim(),
-    OneLetter = removingSpaces[0].toUpperCase();
-    
-  return OneLetter + removingSpaces.slice(1).toLowerCase();
-    
-}
+    return OneLetter + removingSpaces.slice(1).toLowerCase();
 
-const getRollbackMessage = function(amount) {
+  },
 
-  switch (true) {
+  getRollbackMessage: function (amount) {
 
-    case amount > 30000:
-      return "Даем скидку в 10%";
-      break;
-  
-    case 15000 < amount && amount <= 30000:
-      return "Даем скидку в 5%";
-      break;
-  
-    case 0 <= amount && amount <= 15000:
-      return "Скидка не предусмотрена!";
-      break;
-  
-    default:
-      return "Что то пошло не так";
+    switch (true) {
+
+      case amount > 30000:
+        return "Даем скидку в 10%";
+        break;
+
+      case 15000 < amount && amount <= 30000:
+        return "Даем скидку в 5%";
+        break;
+
+      case 0 <= amount && amount <= 15000:
+        return "Скидка не предусмотрена!";
+        break;
+
+      default:
+        return "Что то пошло не так";
+    }
+
+  },
+
+  loger: function () {
+    console.log(appData.additionalServiceOne, appData.additionalServiceTwo);;
+    console.log(appData.screens.toLowerCase().split(/\s* \s*/));
+    console.log(`${appData.fullPrice} Стоимость разработки`);
+    console.log(`Стоимость разработки сайта c учетом вычета отката: ${appData.servicePercentPrice} рублей`);
+
+    for (let key in appData) {
+      if (typeof appData[key] === "function") console.log(`Метод обьекта ${appData[key]} %c Тип ${typeof appData[key]}`, "color: red;");
+      else console.log(`Свойства обьекта ${key} Тип: ${typeof key}`);
+    }
+
+  },
+
+  start: function () {
+    appData.title = prompt("Как называется ваш проект?", "Lesson"),
+      appData.screens = prompt("Какие типы экранов нужно разработать??", "Простые, Сложные, Интерактивные");
+
+    do {
+      appData.screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+    } while (!appData.isNumber(appData.screenPrice));
+
+    appData.screenPrice = parseFloat(appData.screenPrice);
+
+    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+
+    appData.allServicePrices = appData.getAllServicePrices();
+    appData.fullPrice = appData.getFullPrice(appData.screenPrice, appData.allServicePrices);
+    appData.servicePercentPrice = appData.getServicePercentPrices(appData.fullPrice, appData.rollback);
+
+    appData.loger();
   }
-  
+
+
 }
-// Переопределение переменных
-asking();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-
-
-console.log(additionalServiceOne, additionalServiceTwo);
-
-console.log(`Типы данных: ${showTypeOf(getTitle(title))}, ${showTypeOf(fullPrice)}, ${showTypeOf(adaptive)}`);
-console.log(screens.toLowerCase().split(/\s* \s*/));
-console.log(getRollbackMessage(fullPrice));
-console.log(`Стоимость разработки сайта c учетом вычета отката: ${getServicePercentPrices(fullPrice, rollback)} рублей`);
+appData.start();;
